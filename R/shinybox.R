2c94e4b9e82e9092727978ab0b75caec669dfd20
Olivier <olivier.celhay@gmail.com>
2021-10-13 12:17:27 -0400

    Added glue::

--- R/shinybox.R
+++ R/shinybox.R
@@ -40,7 +40,7 @@ shinybox <- function(app_name = "HAL",
   # Determine Operating System ----------------------------------------------
   os <- get_os()
   assign("os", os, envir = .GlobalEnv)  # otherwise object 'os' not found
-  message(glue("Operating System: {os}"))
+  message(glue::glue("Operating System: {os}"))
   
   # Check and fail early ---------------------------------------------------
   check_first(app_name,

d3ab78d0ef6bc25916b9720f7781871af02f7cb0
Olivier <olivier.celhay@gmail.com>
2022-03-09 17:40:08 -0500

    Add icons to test app

--- misc/test_shinybox.R
+++ misc/test_shinybox.R
@@ -4,11 +4,6 @@ detach("package:shinybox", unload = TRUE)
 remotes::install_github("ocelhay/shinybox", ref = "master", upgrade = "never")
 library(shinybox)
 
-# Test shinyboxtestapp ----
-# remove.packages("shinyboxtestapp")
-# library(shinyboxtestapp)
-# shinyboxtestapp::run_app()
-
 # shinyboxtestapp, macOS
 build_path <- glue::glue("/Users/olivier/Desktop/shinybox_{format(Sys.time(), '%Y-%m-%d_%H%M')}")
 dir.create(build_path)
@@ -18,13 +13,12 @@ shinybox(
   author = "Stanley K.",
   description = "Heuristically Programmed ALgorithmic Computer",
   semantic_version = "v1.0.0",
-  cran_like_url = "https://cran.r-project.org",  # "https://cran.microsoft.com/snapshot/2021-01-10",
+  cran_like_url = "https://cran.r-project.org",
   # mac_file = "/Users/olivier/Documents/Ressources Pro/R/macOS targz/R-4.1-branch.tar.gz",
   mac_r_url = "https://mac.r-project.org/high-sierra/R-4.1-branch/x86_64/R-4.1-branch.tar.gz", # only used if mac_file is NULL
   git_host = "github",
-  git_repo = "ocelhay/shinyboxtestapp",
+  git_repo = "ocelhay/shinybox/misc/shinyboxtestapp",
   function_name = "run_app", 
-  local_package_path = NULL,
   package_install_opts = list(type = "binary"),
   build_path = build_path,
   rtools_path_win = NULL,
@@ -43,11 +37,9 @@ shinybox(
   description = "Heuristically Programmed ALgorithmic Computer",
   semantic_version = "v1.0.0",
   cran_like_url = "https://cran.r-project.org",
-  # cran_like_url = "https://cran.microsoft.com/snapshot/2021-01-10",
   git_host = "github",
-  git_repo = "ocelhay/shinyboxtestapp",
+  git_repo = "ocelhay/shinybox/misc/shinyboxtestapp",
   function_name = "run_app", 
-  local_package_path = NULL,
   package_install_opts = list(type = "binary"),
   build_path = build_path,
   rtools_path_win = "C:\\rtools40\\usr\\bin",
